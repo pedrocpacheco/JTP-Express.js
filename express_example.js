44adb1449dfc09458491c127cb67f510de331211
pedrocpacheco <cpedropacheco@hotmail.com>
2024-02-09 12:58:16 -0300

    feat: baisico do express

--- express_example.js
+++ express_example.js
@@ -1,10 +1,7 @@
 let express = require('express')
-
 let app = express()
 
-app.get('/', (req, res) => {
-  res.send("Welcome")
-})
+app.get('/', (req, res) => { res.send('Welcome to JavaTPoint') })
 
 const PORT = 8000
 
